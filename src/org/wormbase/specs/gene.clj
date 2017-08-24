1835074a31c890ac8e2674078c6e0e8af93dc8dc
Matt Russell <matt@mgrbyte.co.uk>
2017-08-24 11:48:13 +0100

    Added doc strings and clean up.

--- src/org/wormbase/specs/gene.clj
+++ src/org/wormbase/specs/gene.clj
@@ -38,7 +38,9 @@
    {:gene/cgc-name #"^[a-z]{3,4}-[1-9]{1}\d*"
     :gene/sequence-name #"^SRAE_[\dXM]\d+$"}})
 
-(defn valid-name? [data name-kw]
+(defn valid-name?
+  "Validates `name-kw` according to correspoinding species in `data`."
+  [data name-kw]
   (let [[_ species] (:gene/species data)
         species-kw (ows/convert-to-ident species)]
     (when-let [patterns (species-kw name-patterns)]
@@ -48,7 +50,9 @@
 
 (def gene-id-regexp #"WBGene\d{8}")
 
-(defn- gen-from-rand-name-pattern [kw]
+(defn- gen-from-rand-name-pattern
+  "Generate names for `kw` a matching random pattern."
+  [kw]
   (let [species-kw (->> (keys name-patterns)
                         (random-sample 0.5)
                         (take 1)
@@ -56,17 +60,26 @@
         pattern (-> name-patterns species-kw kw)]
     (sg/string-generator pattern)))
 
+(defn matches-some-pattern?
+  "Returns a non-nil value if `value` for `name-kw`
+  matches a pattern for any species."
+  [name-kw value]
+  (->> (vals name-patterns)
+       (map name-kw)
+       (some #(re-matches % value))))
+
 (s/def :gene/id
   (s/with-gen
     (s/and st/string? (partial re-matches gene-id-regexp))
     #(sg/string-generator gene-id-regexp)))
 
-(s/def :gene/cgc-name
-  (s/spec (s/and st/string? not-empty)
-          :gen (partial gen-from-rand-name-pattern :gene/cgc-name)))
+(defn name-spec-with-gen [name-kw]
+  (s/spec (s/and st/string? (partial matches-some-pattern? name-kw))
+          :gen (partial gen-from-rand-name-pattern name-kw)))
 
-;; TODO add proper spec 
-(s/def :gene/sequence-name st/string?)
+(s/def :gene/cgc-name (name-spec-with-gen :gene/cgc-name))
+
+(s/def :gene/sequence-name (name-spec-with-gen :gene/sequence-name))
 
 ;; TODO add proper spec
 (s/def :gene/biotype st/keyword?)
