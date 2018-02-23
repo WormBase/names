251a4089e44fd25422ca552173d7e9ae2eebf6b6
Sibyl <sibyl@wormbase.org>
2018-02-23 15:28:14 -0500

    add routes and placeholder pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route , Link } from 'react-router-dom';
 import logo from './logo.svg';
 import wormbaseLogo from './logo_wormbase_solid.svg';
 import './App.css';
@@ -7,14 +8,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <img src={wormbaseLogo} alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>a header and sidebar</p>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/gene/new">Create a new gene</Link></li>
+          <li><Link to="/gene/id/WB1">Edit an exiting gene (example)</Link></li>
+          <li><Link to="/gene/merge">Merge two genes</Link></li>
+          <li><Link to="/gene/split">Split a gene</Link></li>
+        </ul>
+        <Route exact path="/" component={() => 'home page' } />
+        <Route path="/gene" component={({match}) => ([
+          <Route path={`${match.url}/new`} component={() => 'form to create new gene'} />,
+          <Route path={`${match.url}/id/:id`} component={() => 'form edit an existing new gene'} />,
+          <Route path={`${match.url}/merge`} component={() => 'form to merge two genes'} />,
+          <Route path={`${match.url}/split`} component={() => 'form to split a gene'} />,
+        ])} />
+        <Route path="/variation" component={() => 'variation page' } />
       </div>
     );
   }
