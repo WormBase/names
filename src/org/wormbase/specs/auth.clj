1c6aa8eca11e378850967b3d4e8653b8ce211b5a
Matt Russell <matthew.russell@wormbase.org>
2018-03-05 10:11:18 +0000

    Removed use of spec.tools for string predicate.

--- src/org/wormbase/specs/auth.clj
+++ src/org/wormbase/specs/auth.clj
@@ -1,8 +1,7 @@
 (ns org.wormbase.specs.auth
   (:require
    [clojure.spec.alpha :as s]
-   [clojure.string :as str]
-   [spec-tools.spec :as sts]))
+   [clojure.string :as str]))
 
 ;;; TODO: perhaps use urly instead of re-inventing wheels?
 ;;;   https://github.com/michaelklishin/urly
@@ -10,19 +9,19 @@
 
 (def https? #(str/starts-with? % "https://"))
 
-(def absolute-uri (s/and sts/string? https?))
+(def absolute-uri (s/and string? https?))
 
-(def relative-uri (or absolute-uri (s/and sts/string? #(str/starts-with? % "/"))))
+(def relative-uri (or absolute-uri (s/and string? #(str/starts-with? % "/"))))
 
 (s/def ::authorize-uri absolute-uri)
 
-(s/def ::access-token-uri (s/and sts/string? https?))
+(s/def ::access-token-uri (s/and string? https?))
 
 (s/def ::redirect-uri relative-uri)
 
-(s/def ::client-secret (s/and sts/string? not-empty))
+(s/def ::client-secret (s/and string? not-empty))
 
-(s/def ::client-id (s/spec (s/and sts/string? not-empty)))
+(s/def ::client-id (s/spec (s/and string? not-empty)))
 
 (s/def ::auth-profile (s/keys :req-un [::client-secret ::client-id]))
 
@@ -35,7 +34,7 @@
                                         ::client-id
                                         ::client-secret]))
 
-(s/def ::authorization (s/and sts/string? #(str/starts-with? "Bearer " %)))
+(s/def ::authorization (s/and string? #(str/starts-with? "Bearer " %)))
 
 (s/def ::bearer-authentication ::authorization)
 
