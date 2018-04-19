f2551d1268ac947e40ff966791bab71ead46aa9c
Sibyl <sibyl@wormbase.org>
2018-04-19 15:16:32 -0400

    avoid input losing focus by preventing BaseForm rerender when content changes * instread track form content in dataStore outside of state * and the HOC of Field listen to dataStore change and update its state

--- client/src/containers/Gene/BaseForm.js
+++ client/src/containers/Gene/BaseForm.js
@@ -2,41 +2,131 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Button, Icon, TextField } from '../../components/elements';
 
+class FormDataStore {
+  constructor(fields) {
+    this.fields = {...fields};
+    this.eventListeners = [];
+  }
+
+  // setEventListener = (fieldId, handler) => {
+  //   this.fields[fieldId] = this.fields[fieldId] || {};
+  //   this.fields[fieldId].changeHandler = handler;
+  // }
+  //
+  // removeEventListener = (fieldId) => {
+  //   if (this.fields[fieldId]) {
+  //       this.fields[fieldId].changeHandler = null;
+  //   }
+  // }
+
+  setEventListener = (fieldId, handler) => {
+    this.eventListeners = [
+      ...this.eventListeners,
+      {
+        fieldId: fieldId,
+        eventHandler: handler,
+      },
+    ];
+  }
+
+  removeEventListener = (fieldId) => {
+    this.eventListeners = this.eventListeners.filter((eventListener) => {
+      return eventListener.fieldId !== fieldId;
+    });
+  }
+
+  getFieldUpdater = (fieldId) => {
+    return (value) => {
+      this.fields = {
+        ...this.fields,
+        [fieldId]: {
+          value: value,
+        },
+      };
+      this.eventListeners.filter((eventListener) => {
+        return eventListener.fieldId === fieldId;
+      }).map((eventListener) => {
+        eventListener.eventHandler(value);
+      });
+    };
+  }
+
+  getField = (fieldId) => {
+    return {
+      ...this.fields[fieldId],
+    };
+  }
+
+  getAllFields = () => {
+    return {
+      ...this.fields,
+    };
+  }
+
+}
+
 class BaseForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       fields: {...props.fields}
     };
+    this.dataStore = new FormDataStore(this.state.fields);
   }
 
-  handleChange = (fieldId) => {
-    return (event) => {
-      const value = event.target.value;
-      this.setState((prevState) => ({
-        fields: {
-          ...prevState.fields,
-          [fieldId]: {
-            value: value,
-          },
-        },
-        focusField: fieldId,
-      }));
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.state = {
+      fields: {...nextProps.fields}
     };
+    this.dataStore = new FormDataStore(nextProps.fields);
+  }
+
+  componentWillUnmount() {
   }
 
   withData = (WrappedComponent, fieldId) => {
-    return (props) => {
-      return (
-        <WrappedComponent
-          {...props}
-          id={fieldId}
-          value={this.state.fields[fieldId] ? this.state.fields[fieldId].value : null}
-          onChange={this.handleChange(fieldId)}
-          inputRef={element => this.inputElement = element}
-        />
-      );
+    const dataStore = this.dataStore;
+    const {value, onChange,} = dataStore.getField(fieldId);
+    class Field extends Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          ...dataStore.getField(fieldId)
+        };
+      }
+
+      componentDidMount() {
+        dataStore.setEventListener(fieldId, (value) => {
+          this.setState({
+            value: value,
+          });
+        });
+      }
+
+      componentWillUnmount() {
+        dataStore.removeEventListener(fieldId);
+      }
+
+      render() {
+        return (
+          <WrappedComponent
+            {...this.props}
+            id={fieldId}
+            value={this.state.value || ''}
+            onChange={
+              (event) => {
+                dataStore.getFieldUpdater(fieldId)(event.target.value)
+              }
+            }
+          />
+        );
+      }
     }
+    return Field;
   }
 
   render() {
@@ -44,6 +134,7 @@ class BaseForm extends Component {
       <form noValidate autoComplete="off">
         {this.props.children({
           withData: this.withData,
+          getAllFieldData: this.dataStore.getAllFields,
         })}
       </form>
     );
