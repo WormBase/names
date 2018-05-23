5bef24068a264e32852e72b2b7daccfb929baa6e
Sibyl <sibyl@wormbase.org>
2018-05-23 16:29:32 -0400

    make GeneAutocomplete controlled

---
+++ client/src/containers/Gene/GeneSearchBox.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import GeneAutocomplete from './GeneAutocomplete';
+
+class GeneSearchBox extends Component {
+  handle = (event) => {
+
+  }
+
+}
