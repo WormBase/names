4fb534ec5b0e83dd06268f4ae5ac96c90808ae33
Sibyl <sibyl@wormbase.org>
2018-05-25 15:30:19 -0400

    add recent activities on gene index page

--- client/src/containers/Gene/index.js
+++ client/src/containers/Gene/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { withStyles, Button, Icon, Page, } from '../../components/elements';
+import { withStyles, Button, Icon, Page, Typography, } from '../../components/elements';
 
 import GeneProfile from './GeneProfile';
 import GeneCreate from './GeneCreate';
 import GeneSearchBox from './GeneSearchBox';
+import RecentActivities from './RecentActivities';
 
 const Gene = (props) => {
   const {classes} = props;
@@ -25,6 +26,8 @@ const Gene = (props) => {
       </div>
       <div className={classes.main}>
         {/* tables and charts */}
+        <Typography variant="title" gutterBottom>Recent activities</Typography>
+        <RecentActivities />
       </div>
       </div>
     </Page>
@@ -44,8 +47,16 @@ const styles = (theme) => ({
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     alignItems: 'center',
-    marginBottom: theme.spacing.unit * 4,
+    marginBottom: theme.spacing.unit * 6,
   },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      width: '80%',
+    },
+  }
 });
 
 export default withStyles(styles)(Gene);
