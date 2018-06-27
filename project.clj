fefd8978c55e63f1f0e7aff8c758137b922ed6b0
Matt Russell <matthew.russell@wormbase.org>
2018-06-27 09:21:45 +0100

    Split dependencies out into deps.edn (uses tools.deps)

--- project.clj
+++ project.clj
@@ -1,58 +1,22 @@
 (defproject wormbase-names "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies
+  :plugins
   [
-   ;;[org.clojure/tools.deps.alpha "0.5.425"]
-   [org.clojure/clojure "1.9.0"]
-   ;; TODO: figure out how to configure logging
-   ;; When this package is added, no logging statements appear in
-   ;; console by default (compojure-api integration)
-   ;; [org.clojure/tools.logging "0.4.0"]
-   [org.clojure/core.cache "0.6.5"]
-   [org.clojure/data.csv "0.1.4"]
-   [org.clojure/tools.cli "0.3.6"]
-   [clojure.java-time "0.3.1"]
-   [aero "1.1.2"]
-   [amazonica "0.3.121"]
-   [bk/ring-gzip "0.2.1"]
-   [buddy/buddy-auth "2.1.0"]
-   [cheshire "5.8.0"]
-   [clj-http "3.8.0"]
-   [com.draines/postal "2.0.2"]
-   [danlentz/clj-uuid "0.1.7"]
-   [environ "1.1.0"]
-   [expound "0.5.0"]
-   [com.google.api-client/google-api-client "1.23.0"]
-   [io.rkn/conformity "0.5.1"]
-   [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
-                                      javax.jms/jms
-                                      com.sun.jdmk/jmxtools
-                                      com.sun.jmx/jmxri]]
-   [me.raynes/fs "1.4.6"]
-   [metosin/compojure-api "2.0.0-alpha19" :exclusions [potemkin
-                                                       instaparse]]
-   [metosin/spec-tools "0.5.1"]
-   [mount "0.1.12"]
-   ;; [phrase "0.3-alpha3"] TODO: use for better API error messages.
-   [ring/ring-codec "1.1.0"]
-   [ring/ring-core "1.6.3"]
-   [ring/ring-defaults "0.3.1"]
-   [ring/ring-jetty-adapter "1.6.3"]
-   [semantic-csv "0.2.1-alpha1"]]
-  :plugins [[lein-environ "1.1.0"]
-            [lein-pprint "1.1.2"]
-            [lein-ring "0.12.0"]]
-  :tools/deps [:system :home :project]
-  :uberjar-name "server.jar"
-  :uberjar {:aot :all}
+   [lein-environ "1.1.0"]
+   [lein-pprint "1.1.2"]
+   [lein-ring "0.12.0"]
+   [lein-tools-deps "0.4.1"]]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :uberjar-name "app.jar"
   :ring {:handler wormbase.names.service/app
          :host "0.0.0.0"
          :init wormbase.names.service/init}
   :source-paths ["src"]
   :resource-paths ["resources"]
   :target-path "target/%s"
-  :main ^:skip-aot wormbase.names.service
+  :main ^:skip-aot wormbase.names.import-genes
   :monkeypatch-clojure-test false
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :aliases {"import-genes" ["run" "-m" "org.wormbase.names.import-genes"]}
   :profiles
   {:provided
@@ -61,18 +25,6 @@
    :dev
    {:aliases {"code-qa" ["do" ["eastwood"] "test"]
               "spec-test" ["run" "-m" "spec-test"]}
-    :dependencies
-    [[org.clojure/test.check "0.9.0"]
-     [org.clojure/tools.trace "0.7.9"]
-     [com.velisco/strgen "0.1.5"]
-     [datomic-schema-grapher "0.0.1"]
-     [javax.servlet/javax.servlet-api "4.0.0"]
-     [ring/ring-devel "1.6.3"]
-     [peridot "0.5.0"]
-     [vvvvalvalval/datomock "0.2.0"]
-     [ring/ring-mock "0.3.2"]
-     [specviz "0.2.4" :exclusions [com.google.guava/guava
-                                   com.google.code.findbugs/jsr305]]]
     :plugins
     [[com.jakemccrary/lein-test-refresh "0.20.0"]
      [jonase/eastwood "0.2.4" :exclusions [org.clojure/clojure]]
