d424b3c75334b5149415fe44766de11e98bf2b50
Matt Russell <matthew.russell@wormbase.org>
2018-07-02 12:22:22 +0100

    Added local docker configuration.

---
+++ scripts/build-appjar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+artefact="docker/app.jar"
+lein with-profile +datomic-pro ring uberjar
+target="target/uberjar/app.jar"
+if [ $? -eq 0 ]; then
+    mv "${target}" "${artefact}"
+    echo "${artefact}"
+    exit 0
+fi
+exit 1
