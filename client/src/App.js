c6229fd20b9867e0fcf6ced371f80d0c2d0e2bdb
Sibyl <sibyl@wormbase.org>
2018-08-03 14:47:25 -0400

    render NotFound when fails to match route

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Route , Redirect, Switch } from 'react-router-dom';
 import 'typeface-roboto';
-import { withStyles, Page, DocumentTitle, } from './components/elements';
+import { withStyles, Page, DocumentTitle, NotFound} from './components/elements';
 import Header, { NavBar } from './containers/Header';
 import Authenticate, { ProfileButton } from './containers/Authenticate';
 import Footer from './containers/Footer';
@@ -25,44 +25,48 @@ class App extends Component {
                 isAuthenticated ? [
                   <NavBar key="nav-bar" />,
                   <div key="content" className={this.props.classes.content}>
-                    <Route exact path="/" component={() => <Redirect to="/gene" /> } />
-                    <Route exact path="/gene" component={() => (
-                      <DocumentTitle title="Gene index">
-                        <Gene />
-                      </DocumentTitle>
-                    )} />
-                    <Route path="/gene" component={({match}) => (
-                      <Switch>
-                        <Route path={`${match.url}/new`} component={() => (
-                          <DocumentTitle title={`Create a gene`}>
-                            <GeneCreate authorizedFetch={authorizedFetch} />
-                          </DocumentTitle>
-                        )} />
-                        <Route path={`${match.url}/id/:id`} component={({match}) => (
-                          <DocumentTitle title={`Gene ${match.params.id}`}>
-                            <GeneProfile
-                              wbId={match.params.id}
-                              authorizedFetch={authorizedFetch}
-                            />
-                          </DocumentTitle>
-                        )} />
-                      </Switch>
-                    )} />
-                    <Route path="/variation" component={() => (
-                      <DocumentTitle title="Variation index">
-                        <Page>Variation page (coming soon ..ish)</Page>
-                      </DocumentTitle>
-                    )} />
-                    <Route path="/feature" component={() => (
-                      <DocumentTitle title="Feature index">
-                        <Page>Feature page (coming soon ..ish)</Page>
-                      </DocumentTitle>
-                    )} />
-                    <Route path="/me" component={() => (
-                      <DocumentTitle title="My profile">
-                        {profile}
-                      </DocumentTitle>
-                    )} />
+                    <Switch>
+                      <Route exact path="/" component={() => <Redirect to="/gene" /> } />
+                      <Route exact path="/gene" component={() => (
+                        <DocumentTitle title="Gene index">
+                          <Gene />
+                        </DocumentTitle>
+                      )} />
+                      <Route path="/gene" component={({match}) => (
+                        <Switch>
+                          <Route path={`${match.url}/new`} component={() => (
+                            <DocumentTitle title={`Create a gene`}>
+                              <GeneCreate authorizedFetch={authorizedFetch} />
+                            </DocumentTitle>
+                          )} />
+                          <Route path={`${match.url}/id/:id`} component={({match}) => (
+                            <DocumentTitle title={`Gene ${match.params.id}`}>
+                              <GeneProfile
+                                wbId={match.params.id}
+                                authorizedFetch={authorizedFetch}
+                              />
+                            </DocumentTitle>
+                          )} />
+                          <Route component={NotFound} />
+                        </Switch>
+                      )} />
+                      <Route path="/variation" component={() => (
+                        <DocumentTitle title="Variation index">
+                          <Page>Variation page (coming soon ..ish)</Page>
+                        </DocumentTitle>
+                      )} />
+                      <Route path="/feature" component={() => (
+                        <DocumentTitle title="Feature index">
+                          <Page>Feature page (coming soon ..ish)</Page>
+                        </DocumentTitle>
+                      )} />
+                      <Route path="/me" component={() => (
+                        <DocumentTitle title="My profile">
+                          {profile}
+                        </DocumentTitle>
+                      )} />
+                      <Route component={NotFound} />
+                    </Switch>
                   </div>
                 ] :
                 login
