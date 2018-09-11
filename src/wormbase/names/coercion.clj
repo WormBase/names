068a28fdee761202de9aafb875bdfc5a79a4927b
Matt Russell <matthew.russell@wormbase.org>
2018-09-11 14:22:47 +0100

    Syntax error

--- src/wormbase/names/coercion.clj
+++ src/wormbase/names/coercion.clj
@@ -1,7 +1,8 @@
 (ns wormbase.names.coercion
-  [compojure.api.coercion.spec :as spec-coercion]
-  [spec-tools.core :as stc]
-  [spec-tools.transform :as stt])
+  (:require
+   [compojure.api.coercion.spec :as spec-coercion]
+   [spec-tools.core :as stc]
+   [spec-tools.transform :as stt]))
 
 ;;; Modified copies of the original transformers:
 ;;; https://github.com/metosin/compojure-api/blob/master/src/compojure/api/coercion/spec.clj#L16-L35
@@ -34,7 +35,7 @@
                     (assoc-in [:body :string :default] string-transformer))]
     (spec-coercion/create-coercion options)))
 
-(def ^:{:doc "Custom coercion that doesn't strip keys from specs during response processing."}
+(def ^{:doc "Custom coercion that doesn't strip keys from specs during response processing."}
   spec (non-stripping-spec-keys-coercion))
 
 
