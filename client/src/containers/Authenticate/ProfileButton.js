2cbe1eeafc9c58d207042eed2457d4ff84e5e338
Sibyl <sibyl@wormbase.org>
2018-09-27 16:21:09 -0400

    format profile button in the header

--- client/src/containers/Authenticate/ProfileButton.js
+++ client/src/containers/Authenticate/ProfileButton.js
@@ -11,7 +11,7 @@ const ProfileButton = (props) => {
       className={props.classes.button}
     >
       <PersonIcon className={props.classes.icon} />
-      Hello, {props.name}
+      Hi, {props.name.split(/\s+/)[0]}
     </Button>
   ) : null;
 };
@@ -23,6 +23,7 @@ ProfileButton.propTypes = {
 const styles = (theme) => ({
   button: {
     color: theme.palette.common.white,
+    textTransform: 'none',
   },
   icon: {
     marginRight: theme.spacing.unit,
