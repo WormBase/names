02a60e2b2c8b7c38426e27472b93c24b0583be17
Matt Russell <matthew.russell@wormbase.org>
2018-11-01 14:43:26 +0000

    Implemented new and update batch ops.

--- wormids/src/wormids/core.clj
+++ wormids/src/wormids/core.clj
@@ -1,10 +1,41 @@
 (ns wormids.core
-  (:refer-clojure :exclude [update])
   (:require
+   [clojure.spec.alpha :as s]
+   [clojure.string :as str]
    [clojure.walk :as w]
-   [datomic.api :as d]
-   [clj-uuid :as uuid]
-   [wormids.util :refer [cas-batch]]))
+   [datomic.api :as d]))
+
+(defn resolve-ref [db m k v]
+  (assoc m
+         k 
+         (cond
+           (and (map? v) (:db/id v))  (:db/id v)
+           (keyword? v) [:db/ident v]
+           (vector? v) (:db/id (d/entity db v))
+           :else v)))
+
+(defn resolve-refs
+  "Resolve datomic db references in entity-map `em`."
+  [db em]
+  (w/postwalk
+      (fn ref-resolve [xs]
+        (if (map? xs)
+          (reduce-kv (partial resolve-ref db) {} xs)
+          xs))
+      em))
+
+(defn cas-batch
+  "Collate a collection of compare-and-swap operations."
+  [db eid data]
+  (let [entity-map (d/pull db '[*] eid)]
+    (some->> data
+             (resolve-refs db)
+             (map (fn gen-cas [[k v]]
+                    (let [old-v (k (resolve-refs db entity-map))]
+                      (when-not (or (nil? v)
+                                    (= old-v v))
+                        [:db/cas eid k old-v v]))))
+             (remove nil?))))
 
 (defn latest-id
   "Get the latest identifier for a given `ident`."
@@ -16,7 +47,13 @@
        (d/history db)
        ident))
 
-(defn parse-digits [identifier]
+(s/fdef parse-digits
+  :args (s/cat :identifier string?)
+  :ret (s/nilable int?)
+  :fn #(str/includes? (:args %) (-> % :ret str)))
+(defn parse-digits
+  "Parse digits from an identifier."
+  [identifier]
   (some->> identifier
            (re-seq #"0*(\d+)")
            (flatten)
@@ -45,15 +82,15 @@
 
   Returns data suitable for passing to `datomic.api/transact`."
   [db template uiident coll]
-  (assert (>= (count coll) 1))
-  (let [lid (latest-id-number db uiident)
-        start-n (inc lid)
-        stop-n (-> coll count (+ start-n))]
-    (->> (range start-n stop-n)
-         (map inc)
-         (map (partial format template))
-         (map (partial array-map uiident))
-         (interleave coll)
-         (partition 2)
-         (map (partial apply merge)))))
+  (let [start-n (latest-id-number db uiident)
+        stop-n (+ (count coll) start-n)]
+    (some->> (range start-n stop-n)
+             (map inc)
+             (map (partial format template))
+             (map (partial array-map uiident))
+             (interleave coll)
+             (partition 2)
+             (map (partial apply merge)))))
+
+
 
