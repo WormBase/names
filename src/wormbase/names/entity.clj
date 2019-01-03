a48b38e2667971d01aab190d4793842e6b6e2a62
Matt Russell <matthew.russell@wormbase.org>
2019-01-03 11:10:06 +0000

    Prefer pull API
    Removed obsolete comment.

--- src/wormbase/names/entity.clj
+++ src/wormbase/names/entity.clj
@@ -10,23 +10,14 @@
   Lookups `identifier` (conformed with `identify-spec`) in the database.
   Returns `nil` when the entity cannot be found."
   [identitfy-spec request identifier]
-  ;;;;; TODO: this isn't good enough anymore.
-  ;;;;        the specs for gene/cgc-name and gene/sequence-name used to be complex enough
-  ;;;;        to determin which attribute to use given the provideed`identifier`,
-  ;;;;        - now specs have been simpified (moved regexp to db), we need to query
-  ;;;;        - the db for a species specific pattern.... (url needs to change to include species?) -
-  ;;;;        - or more likely, need to match against every species regexp patttern combo...
-  ;;;;        YIKES.
-  ;;;;        without this, the /gene/<foo> won't auto-resolve anymore for sequence-names,
-  ;;;;        as the or-specs default to CGC (being listed first in most or-spec defs)
   (when-not (s/valid? identitfy-spec identifier)
     (throw (ex-info "Found one or more invalid identifiers."
                     {:problems (s/explain-data identitfy-spec identifier)
                      :type ::validation-error})))
   (let [lookup-ref (s/conform identitfy-spec identifier)
         db (:db request)
-        ent (d/entity db lookup-ref)]
-    (if ent
+        ent (d/pull db '[*] lookup-ref)]
+    (if (:db/id ent)
       [lookup-ref ent]
       (not-found! {:message "Entity lookup failed"
                    :lookup-ref lookup-ref}))))
