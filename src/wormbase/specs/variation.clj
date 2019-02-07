14810088e405f5c22063a5d0021c3a9e64b56a78
Matt Russell <matthew.russell@wormbase.org>
2019-02-07 11:56:11 +0000

    Added specs for variation attributes.

--- src/wormbase/specs/variation.clj
+++ src/wormbase/specs/variation.clj
@@ -1,2 +1,13 @@
-(ns wormbase.specs.variation)
+(ns wormbase.specs.variation
+  (:require
+   [clojure.spec.alpha :as s]))
 
+(def id-regexp #"WBVar\d{8}")
+
+(def name-regexp #"(([a-z]+)(Df|Dp|Ti|T|Is)?([1-9]+))*")
+
+(s/def :variation/id (s/and string? (partial re-matches id-regexp)))
+
+(s/def :variation/name (s/and string? (partial re-matches name-regexp)))
+
+(s/def :variation/status keyword?)
