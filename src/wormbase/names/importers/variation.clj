c518d17be1fa977e10281087362570d99e949e42
Matt Russell <matthew.russell@wormbase.org>
2019-02-07 11:45:23 +0000

    Implemented variation import.
    - Added event types to seed data.
    - Implement variation import with constant provenance.

--- src/wormbase/names/importers/variation.clj
+++ src/wormbase/names/importers/variation.clj
@@ -1,15 +1,19 @@
 (ns wormbase.names.importers.variation
   (:require
-   [wormbase.names.importers.processing :as wnip]
    [clojure.java.io :as io]
-   [clojure.spec.alpha :as s]))
+   [clojure.spec.alpha :as s]
+   [wormbase.names.importers.processing :as wnip]
+   [wormbase.specs.variation])
+  (:import
+   (java.util Date)))
 
 (def ^{:doc "A mapping describing the format of the export files."}
   ;; no provenance exported for variations
   export-conf {:data {:header [:variation/id
-                               :variation/name
-                               :variation/status]}})
+                               :variation/status
+                               :variation/name]}})
 
+(s/def ::lax-name (s/and string? not-empty))
 
 (s/def ::status (s/and string?
                        (s/or :variation.status/live (partial = "Live")
@@ -17,25 +21,36 @@
 
 (def transact-batch (partial wnip/transact-batch :event/import))
 
+(defn drop-name-maybe [m]
+  (if (apply = (vals (select-keys m [:variation/id :variation/name])))
+    (dissoc m :variation/name)
+    m))
+
 (defn build-data-txes
   "Build the current entity representation of each variation."
   [tsv-path conf
    & {:keys [batch-size]
       :or {batch-size 500}}]
   (let [cast-fns {:variation/id (partial wnip/conformed :variation/id)
-                  :variation/name (partial wnip/conformed :variation/name)
+                  :variation/name (partial wnip/conformed ::lax-name)
                   :variation/status (partial wnip/conformed-label ::status)}]
     (with-open [in-file (io/reader tsv-path)]
       (->> (wnip/parse-transform-cast in-file conf cast-fns)
+           (map drop-name-maybe)
            (map wnip/discard-empty-valued-entries)
            (partition-all batch-size)
            (doall)))))
 
 
 (defn batch-transact-data [conn tsv-path]
-  (let [conf (:data export-conf)]
+  (let [conf (:data export-conf)
+        prov {:db/id "datomic.tx"
+              :provenance/what :event/import-variation
+              :provenance/who [:person/email "paul.davis@wormbase.org"]
+              :provenance/when (Date.)
+              :provenance/how :agent/importer}]
     (doseq [batch (build-data-txes tsv-path conf)]
-      @(transact-batch conn batch))))
+      @(transact-batch conn (conj batch prov)))))
   
 (defn process
   [conn data-tsv-path & {:keys [n-in-flight]
