56e5adaf6d8c78b804cfd1c1455872113a8fc464
Sibyl <sibyl@wormbase.org>
2019-03-29 14:45:23 -0400

    NavBar based on ENTITY_TYPES

--- client/src/containers/Header/NavBar.js
+++ client/src/containers/Header/NavBar.js
@@ -4,22 +4,13 @@ import { withRouter } from 'react-router-dom';
 import { matchPath } from 'react-router';
 
 import { Tabs, Tab, withStyles } from '../../components/elements';
+import { capitalize } from '../../utils/format';
+import { ENTITY_TYPES } from '../../utils/entityTypes';
 
 const NavBar = (props) => {
-  const tabs = [
-    {
-      name: 'Gene',
-      value: '/gene',
-    },
-    {
-      name: 'Variation',
-      value: '/variation',
-    },
-  ];
-
-  const currentTab = tabs.filter((tab) =>
+  const currentTab = ENTITY_TYPES.filter((entityType) =>
     matchPath(props.location.pathname, {
-      path: tab.value,
+      path: `/${entityType}`,
       exact: false,
       strict: false,
     })
@@ -27,13 +18,17 @@ const NavBar = (props) => {
 
   return (
     <Tabs
-      value={currentTab ? currentTab.value : false}
-      onChange={(event, value) => props.history.push(value)}
+      value={currentTab ? currentTab : false}
+      onChange={(event, value) => props.history.push(`/${value}`)}
       centered={true}
       className={props.classes.root}
     >
-      {tabs.map((tab) => (
-        <Tab key={tab.name} label={tab.name} value={tab.value} />
+      {ENTITY_TYPES.map((entityType) => (
+        <Tab
+          key={entityType}
+          label={capitalize(entityType)}
+          value={entityType}
+        />
       ))}
     </Tabs>
   );
