6b2e0fc2d5fcbfff19ff2cf10f84f7529e7e6b98
Matt Russell <matthew.russell@wormbase.org>
2019-04-12 10:23:53 +0100

    Added missing tests for variation summary and finding.

---
+++ test/integration/test_find_variation_by_any_name.clj
@@ -0,0 +1,87 @@
+(ns integration.test-find-variation-by-any-name
+  (:require
+   [clojure.spec.gen.alpha :as gen]
+   [clojure.string :as str]
+   [clojure.test :as t]
+   [wormbase.db-testing :as db-testing]
+   [wormbase.fake-auth :as fake-auth]
+   [wormbase.gen-specs.variation :as gsv]
+   [wormbase.names.service :as service]
+   [wormbase.test-utils :as tu]))
+
+(t/use-fixtures :each db-testing/db-lifecycle)
+
+(defn make-auth-payload
+  [& {:keys [current-user]
+      :or {current-user "tester@wormbase.org"}}]
+  (fake-auth/payload {"email" current-user}))
+
+(defn find-variation
+  [pattern & {:keys [current-user]
+              :or {current-user "tester@wormbase.org"}}]
+  (binding [fake-auth/*gapi-verify-token-response* (fake-auth/payload {"email" current-user})]
+    (let [params {:pattern pattern}
+          headers {"accept" "application/json"
+                   "authorization" "Token IsTotallyMadeUp"}
+          [status body] (tu/get*
+                         service/app
+                         "/api/variation/"
+                         params
+                         headers)]
+      [status (tu/parse-body body)])))
+
+(defn- rand-prefix [string]
+  (subs string 0 (max 1 (-> (rand)
+                            (* (count string))
+                            (Math/floor)
+                            (int)))))
+
+(t/deftest find-by-any-name
+  (t/testing "Get an validation errror (400) result for invalid find terms."
+    (doseq [term [""]]
+      (let [[status body] (find-variation term)]
+        (tu/status-is? 400 status body)
+        (t/is (not (contains? body :matches)))
+        (t/is (re-matches #".*validation failed.*"
+                          (get body :message "")))
+        (t/is (:problems body) (pr-str body)))))
+  (let [ids (gen/sample gsv/id 2)
+        names (gen/sample gsv/name 2)
+        stati (repeat 2 :variation.status/live)
+        fixtures (map #(zipmap [:variation/id :variation/name :variation/status] [%1 %2 %3])
+                      ids names stati)]
+    (tu/with-fixtures
+      fixtures
+      (fn test-find-cases [conn]
+        (t/testing "Get a 200 response for a non-matching find term"
+          (let [[status body] (find-variation "foobar")
+                matches (:matches body)]
+            (tu/status-is? 200 status body)
+            (t/is (empty? matches))))
+        (t/testing "Whitepace at begining and end of find term is ignoreed"
+          (doseq [term [" foo" "bar "]]
+            (let [[status body] (find-variation term)
+                  matches (:matches body)]
+              (tu/status-is? 200 status body)
+              (t/is (empty? matches)))))
+        (t/testing "Results found for matching variation prefixes"
+          (let [sample (rand-nth fixtures)
+                id (:variation/id sample)
+                value (:variation/name sample)]
+            (when value
+              (let [valid-prefix (rand-prefix value)
+                    [status body] (find-variation valid-prefix)
+                    matches (:matches body)]
+                (tu/status-is? 200 status body)
+                (t/is (seq matches)
+                      (str "No matches found for " valid-prefix))
+                (t/is (some (fn [match] (= (:variation/id match) id)) matches)
+                      (str "Could not find any Variation ID matching " id
+                           " matches:" (pr-str matches)))
+                (t/is (some (fn [match]
+                              (assert (not (nil? match)))
+                              (str/starts-with? (:variation/name match) valid-prefix))
+                            matches)
+                      (str "Could not verify and match startswith prefix "
+                           (pr-str valid-prefix) ""
+                           (pr-str body)))))))))))
---
+++ test/integration/test_variation_summary.clj
@@ -0,0 +1,43 @@
+(ns integration.test-variation-summary
+  (:require
+   [clojure.spec.gen.alpha :as gen]
+   [clojure.test :as t]
+   [ring.util.http-predicates :as ru-hp]
+   [ring.util.http-response :refer [not-found ok]]
+   [wormbase.fake-auth :as fake-auth]
+   [wormbase.gen-specs.variation :as gsv]   
+   [wormbase.test-utils :as tu]
+   [wormbase.db-testing :as db-testing]
+   [wormbase.names.service :as service]
+   [wormbase.api-test-client :as api-tc]))
+
+(t/use-fixtures :each db-testing/db-lifecycle)
+
+(defn- gen-sample []
+  (let [id (first  (gen/sample gsv/id 1))
+        name (first (gen/sample gsv/name 1))
+        status :variation.status/live
+        sample {:variation/status status
+                :variation/name name
+                :variation/id id}]
+    [id sample]))
+
+(def summary (partial api-tc/summary "variation"))
+
+(t/deftest test-summary
+  (t/testing "Summary of a live variation can be retrieved by WBVar ID."
+    (let [[id data-sample] (gen-sample)]
+      (tu/with-fixtures
+        data-sample
+        (fn check-variation-summary [conn]
+          (let [[status body] (summary id)]
+            (tu/status-is? (:status (ok)) status body)))))))
+
+(t/deftest maltformed-identifier
+  (t/testing "A malformed identifier results in a 404 response."
+    (let [[id data-sample] (gen-sample)]
+      (tu/with-fixtures
+        data-sample
+        (fn check-variation-summary [conn]
+          (let [[status body] (summary "WBGene0123123123")]
+            (t/is (ru-hp/bad-request? {:status status :body body}))))))))
