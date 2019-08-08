7e01422d22933b3737cbd9e0d4512cc0619e4d1a
Sibyl <sibyl@wormbase.org>
2019-08-08 14:36:52 -0400

    fixes #234 populate species dropdown from api

--- client/src/components/elements/SpeciesSelect.js
+++ client/src/components/elements/SpeciesSelect.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useContext, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { MenuItem, withStyles } from '@material-ui/core';
 import TextField from './TextField';
+import { mockFetchOrNot } from '../../mock';
+import {
+  AuthorizationContext,
+  useDataFetch,
+} from '../../containers/Authenticate';
 
 const SpeciesSelect = (props) => {
+  const { authorizedFetch } = useContext(AuthorizationContext);
+  const memoizedFetchFunc = useCallback(
+    () => () =>
+      mockFetchOrNot(
+        (mockFetch) => {
+          return mockFetch.get('*', [
+            {
+              'species/cgc-name-pattern': '^[a-z]{3,4}-[1-9]\\d*(\\.\\d+)?$',
+              'species/id': 'species/c-elegans',
+              'species/latin-name': 'Caenorhabditis elegans',
+              'species/sequence-name-pattern':
+                '^[A-Z0-9_cel]+\\.[1-9]\\d{0,3}[A-Za-z]?$',
+            },
+            {
+              'species/cgc-name-pattern':
+                '^Cbr-[a-z21]{3,4}-[1-9]\\d*(\\.\\d+)?$',
+              'species/id': 'species/c-briggsae',
+              'species/latin-name': 'Caenorhabditis briggsae',
+              'species/sequence-name-pattern': '^CBG\\d{5}$',
+            },
+          ]);
+        },
+        () =>
+          authorizedFetch(`/api/species`, {
+            method: 'GET',
+          })
+      ),
+    [authorizedFetch]
+  );
+
+  const { data } = useDataFetch(memoizedFetchFunc, []);
   const SPECIES = [
     'Caenorhabditis elegans',
-    'Caenorhabditis briggsae',
-    'Caenorhabditis remanei',
-    'Caenorhabditis brenneri',
-    'Pristionchus pacificus',
-    'Caenorhabditis japonica',
-    'Brugia malayi',
-    'Onchocerca volvulus',
-    'Strongyloides ratti',
+    ...data
+      .map((species) => species['species/latin-name'])
+      .filter((speciesName) => speciesName !== 'Caenorhabditis elegans')
+      .sort(),
   ];
   const speciesOptions = props.required ? [...SPECIES] : [null, ...SPECIES];
   const { classes, ...others } = props;
