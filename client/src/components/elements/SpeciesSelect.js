52a76fb43f297602bc1afbee3cf6ff02bd70a817
Sibyl <sibyl@wormbase.org>
2019-10-09 17:46:36 -0400

    get species working

--- client/src/components/elements/SpeciesSelect.js
+++ client/src/components/elements/SpeciesSelect.js
@@ -12,18 +12,17 @@ const SpeciesSelect = (props) => {
         (mockFetch) => {
           return mockFetch.get('*', [
             {
-              'species/cgc-name-pattern': '^[a-z]{3,4}-[1-9]\\d*(\\.\\d+)?$',
-              'species/id': 'species/c-elegans',
-              'species/latin-name': 'Caenorhabditis elegans',
-              'species/sequence-name-pattern':
+              'cgc-name-pattern': '^[a-z]{3,4}-[1-9]\\d*(\\.\\d+)?$',
+              id: 'species/c-elegans',
+              'latin-name': 'Caenorhabditis elegans',
+              'sequence-name-pattern':
                 '^[A-Z0-9_cel]+\\.[1-9]\\d{0,3}[A-Za-z]?$',
             },
             {
-              'species/cgc-name-pattern':
-                '^Cbr-[a-z21]{3,4}-[1-9]\\d*(\\.\\d+)?$',
-              'species/id': 'species/c-briggsae',
-              'species/latin-name': 'Caenorhabditis briggsae',
-              'species/sequence-name-pattern': '^CBG\\d{5}$',
+              'cgc-name-pattern': '^Cbr-[a-z21]{3,4}-[1-9]\\d*(\\.\\d+)?$',
+              id: 'species/c-briggsae',
+              'latin-name': 'Caenorhabditis briggsae',
+              'sequence-name-pattern': '^CBG\\d{5}$',
             },
           ]);
         },
@@ -39,7 +38,7 @@ const SpeciesSelect = (props) => {
   const SPECIES = [
     'Caenorhabditis elegans',
     ...data
-      .map((species) => species['species/latin-name'])
+      .map((species) => species['latin-name'])
       .filter((speciesName) => speciesName !== 'Caenorhabditis elegans')
       .sort(),
   ];
