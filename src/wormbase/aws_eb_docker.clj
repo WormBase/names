c390fd4fc86d70e57bc42687be3b8f3f3758bc7d
Matt Russell <matthew.russell@wormbase.org>
2019-10-24 16:13:51 +0100

    Use an edn resource file to store project metadata.

--- src/wormbase/aws_eb_docker.clj
+++ src/wormbase/aws_eb_docker.clj
@@ -1,20 +1,22 @@
 (ns wormbase.aws-eb-docker
   (:require
    [clojure.string :as str]
-   [wormbase.names.metav :as wn-metav]))
+   [wormbase.util :as wu]))
 
 (def proj-pattern #"wormbase/names:[\d.]+")
 
 (defn replace-version [content new-version]
   (str/replace content proj-pattern (str "wormbase/names:" new-version)))
 
-(defn update-eb-json! [eb-json-path]
+(defn update-eb-json! [eb-json-path new-version]
   (let [curr-content (slurp eb-json-path)
         new-content (slurp eb-json-path)]
     (spit eb-json-path (-> eb-json-path
                            slurp
-                           (replace-version wn-metav/version)))))
+                           (replace-version new-version)))))
 (defn -main
   [& args]
-  (println wn-metav/version)
-  (update-eb-json! "./Dockerrun.aws.json"))
+  (let [proj-meta (wu/read-app-config "meta.edn")
+        version (:version proj-meta)]
+    (println version)
+    (update-eb-json! version "./Dockerrun.aws.json")))
