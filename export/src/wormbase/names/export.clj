8a1350fd4b4be6907940b6baa755df5f0b13a3e4
Matt Russell <matthew.russell@wormbase.org>
2020-01-21 11:43:01 +0000

    Amended wording in console output.

--- export/src/wormbase/names/export.clj
+++ export/src/wormbase/names/export.clj
@@ -185,7 +185,8 @@
       (:list options) (exit 0 (list-available-entitiy-types db))
       (nil? db-uri) (exit 1 (str "Please set the WB_DB_URI environment variable.\n"
                                  "e.g: export WB_DB_URI=\"datomic:ddb://us-east-1/WSNames/20190503_12\""))
-      (nil? ent-ns) (exit 1 (str "Please specify an entity type to export. Use --list to available types."))
+      (nil? ent-ns) (exit 1 (str "Please specify an entity type to export. "
+                                 "Use --list to see the available types."))
       (nil? out-path) (exit 1 "Specify the output file path. e.g: /tmp/foo.csv"))
     (run db ent-ns out-path)
     (d/release conn)
