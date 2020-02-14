83b1a6cb0524d4c4bfba8f2162741c999272d5a0
Matt Russell <matthew.russell@wormbase.org>
2020-02-14 15:02:18 +0000

    Do not specify primary identifiers in an update.
    (satisfy change to using datomic :db.unique/value constraint)

--- src/wormbase/names/entity.clj
+++ src/wormbase/names/entity.clj
@@ -173,18 +173,19 @@
           (let [resolve-refs-to-db-ids (or ref-resolver-fn
                                            (fn noop-resolver [_ data]
                                              data))
-               cdata (->> data
-                          (resolve-refs-to-db-ids db)
-                          (into {})
-                          (prepare-data-for-transact db))
-               prov (wnp/assoc-provenance request payload event)
-               txes [['wormbase.ids.core/cas-batch lur cdata] prov]
-               tx-result @(d/transact-async conn txes)]
-          (when-let [db-after (:db-after tx-result)]
-            (if-let [updated (wdb/pull db-after summary-pull-expr lur)]
-              (ok {:updated (wnu/unqualify-keys updated ent-ns)})
-              (not-found
-               (format "%s '%s' does not exist" ent-ns (last lur)))))))))))
+                cdata (dissoc (->> data
+                                   (resolve-refs-to-db-ids db)
+                                   (into {})
+                                   (prepare-data-for-transact db))
+                              uiident)
+                prov (wnp/assoc-provenance request payload event)
+                txes [['wormbase.ids.core/cas-batch lur cdata] prov]
+                tx-result @(d/transact-async conn txes)]
+            (when-let [db-after (:db-after tx-result)]
+              (if-let [updated (wdb/pull db-after summary-pull-expr lur)]
+                (ok {:updated (wnu/unqualify-keys updated ent-ns)})
+                (not-found
+                 (format "%s '%s' does not exist" ent-ns (last lur)))))))))))
 
 (defn status-changer
   "Return a handler to change the status of an entity to `to-status`.
