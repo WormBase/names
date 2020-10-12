WormBase/names

commit d6b6329f937b9aaf88be7d0e6fed3807f75d02c6

Added fn to ids package for local debug printing

ids/src/wormbase/ids/core.clj
@@ -5,6 +5,12 @@
    [clojure.walk :as w]
    [datomic.api :as d]))
 
+(def log-file "/home/mlp/logs/wormbase-ids-core.log")
+(spit log-file "")
+(defn debug-log
+  [log-str]
+  (spit log-file (str log-str "\n") :append true))
+
 (defn attr-schema-unique? [db attr]
   (try
     (let [s-attr (:db/unique (d/entity db attr))]

commit 11de45f2656d3bc7d6a79373c241b2b245b2c065

Fixed debug-log initiation code causing problems in docker deployments (introduced in d6b6329f)

ids/src/wormbase/ids/core.clj
@@ -5,8 +5,16 @@
    [clojure.walk :as w]
    [datomic.api :as d]))
 
-(def log-file "/home/mlp/logs/wormbase-ids-core.log")
-(spit log-file "")
+;In order to use these functions for debugging:
+; 1. Adjust the code to initialise a local debug log file (below)
+; (def log-file "/home/mlp/logs/wormbase-ids-core.log")
+; (init-empty-debug-log log-file)
+; 2. Add debug-log statements in any wormbase.ids code to debug
+; (debug-log "print statement")
+(def log-file nil)
+(defn init-empty-debug-log
+  [log-file]
+  (spit log-file ""))
 (defn debug-log
   [log-str]
   (spit log-file (str log-str "\n") :append true))